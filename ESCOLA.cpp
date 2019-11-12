153543e22561cf81e982b07dbae4ace9d2951225
Tiago Pereira <tiago.peng@gmail.com>
2019-11-12 07:28:16 -0300

    listar professores

--- ESCOLA.cpp
+++ ESCOLA.cpp
@@ -4,15 +4,15 @@
 /*
 
 
-Desenvolver um programa em C que controle o funcionamento de uma Escola. Existem diversas funcionalidades em uma Escola, mas você deve focar no que é pedido. Caso tenha interesse você pode evoluir sua solução para adicionar mais funcionalidades.
+Desenvolver um programa em C que controle o funcionamento de uma Escola. Existem diversas funcionalidades em uma Escola, mas vocï¿½ deve focar no que ï¿½ pedido. Caso tenha interesse vocï¿½ pode evoluir sua soluï¿½ï¿½o para adicionar mais funcionalidades.
 Funcionalidades solicitadas:
 
-Cadastro de Alunos (Matrícula, Nome, Sexo, Data Nascimento, CPF) .
-Cadastro de Professores (Matrícula, Nome, Sexo, Data Nascimento, CPF).
-Cadastro de Disciplinas (Nome, Código, Semestre, Professor)
+Cadastro de Alunos (Matrï¿½cula, Nome, Sexo, Data Nascimento, CPF) .
+Cadastro de Professores (Matrï¿½cula, Nome, Sexo, Data Nascimento, CPF).
+Cadastro de Disciplinas (Nome, Cï¿½digo, Semestre, Professor)
 Inserir/Excluir aluno de uma disciplina
 
-Relatórios
+Relatï¿½rios
 Listar Alunos
 Listar Professores
 Listar Disciplinas (dados da disciplina sem os alunos)
@@ -23,36 +23,40 @@ Listar Alunos ordenados por data de nascimento
 Listar Professores por sexo (Masculino/Feminino)
 Listar Professores ordenados por Nome
 Listar Professores ordenados por data de nascimento
-Aniversariantes do mês
-Lista de pessoas (professor/aluno) a partir de uma string de busca. O usuário informa no mínimo três letras e deve ser listado todas as pessoas que contem essas três letras no nome.
+Aniversariantes do mï¿½s
+Lista de pessoas (professor/aluno) a partir de uma string de busca. O usuï¿½rio informa no mï¿½nimo trï¿½s letras e deve ser listado todas as pessoas que contem essas trï¿½s letras no nome.
 
-Observação:
+Observaï¿½ï¿½o:
 Por Cadastro entenda Incluir, excluir, atualizar
-Valide todos os campos necessários (ex. Data nascimento, CPF)
+Valide todos os campos necessï¿½rios (ex. Data nascimento, CPF)
 
 */
 
-typedef struct dma {
-  int dia;
-  int mes;
-  int ano;
+typedef struct dma
+{
+    int dia;
+    int mes;
+    int ano;
 } Data;
 
-typedef struct {
+typedef struct
+{
 
     char nome[20], cpf[12], sexo, matricula[10];
     Data data_nascimento;
 
 } Fichaaluno;
 
-typedef struct {
+typedef struct
+{
 
     char nome[20], cpf[12], sexo, matricula[10];
     Data data_nascimento;
 
 } Fichaprofessor;
 
-typedef struct {
+typedef struct
+{
 
     char nome[20], codigo[10], professor[20];
     int semestre;
@@ -67,18 +71,32 @@ Fichaprofessor cadastraProfessor();
 
 Fichadisciplina cadastraDisciplina();
 
-
-void listarAlunos(Fichaaluno *alunos, int qtdAlunos){
+void listarAlunos(Fichaaluno *alunos, int qtdAlunos)
+{
 
     int i;
 
-    for(i=0; i<qtdAlunos; i++){
+    for (i = 0; i < qtdAlunos; i++)
+    {
 
-        printf("\n\nNOME DO ALUNO: %s", alunos[i].nome);
+        printf("\nNOME DO ALUNO: %s", alunos[i].nome);
         printf("CPF DO ALUNO: %s", alunos[i].cpf);
         printf("\nMATRICULA DO ALUNO: %s", alunos[i].matricula);
-        printf("\nSEXO DO ALUNO: %c\n", alunos[i].sexo);
-        printf("\DATA DE NASCIMENTO: %d/%d/%d\n", alunos[i].data_nascimento.dia,alunos[i].data_nascimento.mes, alunos[i].data_nascimento.ano );
+        printf("SEXO DO ALUNO: %c", alunos[i].sexo);
+        printf("DATA DE NASCIMENTO: %d/%d/%d\n", alunos[i].data_nascimento.dia, alunos[i].data_nascimento.mes, alunos[i].data_nascimento.ano);
+    }
+}
+
+void listarDisciplinas(Fichadisciplina *disciplinas, int qtdDisciplnas)
+{
+    int i;
+
+    for (i = 0; i < qtdDisciplnas; i++)
+    {
+        printf("\nNOME DA DISCIPLINA: %s", disciplinas[i].nome);
+        printf("CODIGO DA DISCIPLINA: %s", disciplinas[i].codigo);
+        printf("NOME DO PROFESSOR: %s", disciplinas[i].professor);
+        printf("SEMESTRE DA DISCIPLINA: %d\n", disciplinas[i].semestre);
     }
 }
 
@@ -98,99 +116,115 @@ void listarAlunos(Fichaaluno *alunos, int qtdAlunos){
 
 */
 
-
 int menuInicial();
 
-int main(){
+int main()
+{
 
     int sair = 0, opcao, qtdAlunos = 0, qtdProfessores = 0, qtdDisciplnas = 0, i = 0;
     Fichaaluno alunos[10];
     Fichaprofessor professores[10];
     Fichadisciplina disciplinas[10];
 
-
-    while(!sair){
+    while (!sair)
+    {
 
         opcao = menuInicial();
 
-        switch(opcao){
+        switch (opcao)
+        {
 
-            case 1:{
-                    alunos[qtdAlunos] = cadastraAluno();
-                    qtdAlunos++;
-                break;
-            }
-            case 2:{
-                    professores[qtdProfessores] = cadastraProfessor();
-                    qtdProfessores++;
-                break;
-            }
-            case 3:{
-                    disciplinas[qtdDisciplnas] = cadastraDisciplina();
-                    qtdDisciplnas++;
-                break;
-            }
-            case 4:{
+        case 1:
+        {
+            alunos[qtdAlunos] = cadastraAluno();
+            qtdAlunos++;
+            break;
+        }
+        case 2:
+        {
+            professores[qtdProfessores] = cadastraProfessor();
+            qtdProfessores++;
+            break;
+        }
+        case 3:
+        {
+            disciplinas[qtdDisciplnas] = cadastraDisciplina();
+            qtdDisciplnas++;
+            break;
+        }
+        case 4:
+        {
 
-                    listarAlunos(alunos, qtdAlunos);
-                
-					                   	
-                break;
-            }
-            //CHAMAR FUNÃ‡ÃƒO EMITIR RELATÃ“RIO; -> INICIAR SUBFUNCOES DE RELATORIOS
-            case 5:{
-                printf("\n| PROJETO ESCOLA FINALIZADO |\n\n");
-                sair = 1;
-                break;
+            listarAlunos(alunos, qtdAlunos);
 
-            default: {
+            break;
+        }
+        //CHAMAR FUNÃ‡ÃƒO EMITIR RELATÃ“RIO; -> INICIAR SUBFUNCOES DE RELATORIOS
+        case 5:
+        {
+            listarDisciplinas(disciplinas, qtdDisciplnas);
 
-             printf("**********\n");
-             printf("OPCAO INVALIDA\n");
-             printf("**********\n");
-break;
-  }
+            break;
+        }
+        case 6:
+        {
+            printf("\n| PROJETO ESCOLA FINALIZADO |\n\n");
+            sair = 1;
+            break;
 
-            }
+        default:
+        {
+
+            printf("**********\n");
+            printf("OPCAO INVALIDA\n");
+            printf("**********\n");
+            break;
+        }
+        }
         }
     }
 
     return 0;
 }
 
-void flush() {
+void flush()
+{
     //funÃ§ao excelente para limpar o buffer de entrada.
     int ch;
-    while ( ( ch = fgetc ( stdin ) ) != EOF && ch != '\n' ) {}
+    while ((ch = fgetc(stdin)) != EOF && ch != '\n')
+    {
+    }
 }
 
-Fichaaluno cadastraAluno(){
+Fichaaluno cadastraAluno()
+{
 
     Fichaaluno aluno;
 
     printf("NOME COMPLETO: ");
     flush();
-    fgets(aluno.nome,20,stdin);
+    fgets(aluno.nome, 20, stdin);
     printf("MATRICULA: ");
     fgets(aluno.matricula, 20, stdin);
     printf("CPF: ");
     fgets(aluno.cpf, 12, stdin);
-    flush();
     printf("SEXO: ");
-    scanf("%c",&aluno.sexo);
+    scanf("%c", &aluno.sexo);
+    flush();
     printf("informe o dia de nascimento \n");
-    scanf ("%d", &aluno.data_nascimento.dia);
+    scanf("%d", &aluno.data_nascimento.dia);
     printf("informe o mes de nascimento \n");
-    scanf ("%d", &aluno.data_nascimento.mes);
+    scanf("%d", &aluno.data_nascimento.mes);
     printf("informe o ano de nascimento \n");
-    scanf ("%d", &aluno.data_nascimento.ano);
+    scanf("%d", &aluno.data_nascimento.ano);
 
     printf("\n| ALUNO CADASTRADO |\n\n");
 
     return aluno;
 }
 
-Fichaprofessor cadastraProfessor(){
+Fichaprofessor cadastraProfessor()
+{
 
     Fichaprofessor professor;
 
@@ -203,22 +237,21 @@ Fichaprofessor cadastraProfessor(){
     fgets(professor.cpf, 12, stdin);
     flush();
     printf("SEXO: ");
-    scanf("%c",&professor.sexo);
-printf("informe o dia de nascimento \n");
-    scanf ("%d", &professor.data_nascimento.dia);
+    scanf("%c", &professor.sexo);
+    printf("informe o dia de nascimento \n");
+    scanf("%d", &professor.data_nascimento.dia);
     printf("informe o mes de nascimento \n");
-    scanf ("%d", &professor.data_nascimento.mes);
+    scanf("%d", &professor.data_nascimento.mes);
     printf("informe o ano de nascimento \n");
-    scanf ("%d", &professor.data_nascimento.ano);
-
-
+    scanf("%d", &professor.data_nascimento.ano);
 
     printf("\n|  PROFESSOR CADASTRADO  |\n");
 
     return professor;
 }
 
-Fichadisciplina cadastraDisciplina(){
+Fichadisciplina cadastraDisciplina()
+{
 
     Fichadisciplina disciplina;
 
@@ -230,20 +263,21 @@ Fichadisciplina cadastraDisciplina(){
     printf("NOME DO PROFESSOR: ");
     fgets(disciplina.professor, 20, stdin);
     printf("SEMESTRE: ");
-    scanf("%d",&disciplina.semestre);
+    scanf("%d", &disciplina.semestre);
 
     printf("\n|  DISCIPLINA CADASTRADA  |\n");
 
     return disciplina;
 }
 
-int menuInicial(){
+int menuInicial()
+{
 
     int opcao;
 
     printf("MENU INICIAL\nDIGITE A OPCAO DESEJADA:\n1 - CADASTRAR ALUNO\n2 - CADASTRAR PROFESSOR\n3 - CADASTRAR DISCIPLINA");
-    printf("\n4 - EMITIR RELATORIO\n5 - SAIR\nDigite o numero da opcao:  ");
-    scanf("%d",&opcao);
+    printf("\n4 - EMITIR RELATORIO\n5 - LISTAR DISCIPLINAS\n6 - SAIR\nDigite o numero da opcao:  ");
+    scanf("%d", &opcao);
 
     return opcao;
 }
@@ -252,4 +286,3 @@ int menuRelatorio(){
 
     printf("1 - LISTAR ALUNOS\n2 - LISTAR PROFESSORES\n3 - LISTAR DISCIPLINAS ");
 }*/
-
